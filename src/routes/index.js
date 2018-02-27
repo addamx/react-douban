b39dc0461957ccee87b58c223d28c6b91fbe6113
addams <addams.x@infinitytechnologies.com.cn>
2018-02-27 19:29:03 +0800

    v

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../pages/Home';
 import Search from '../pages/Search';
 import MovieSearch from '../pages/Search/MovieSearch';
@@ -10,8 +10,10 @@ import Rank from '../pages/Rank';
 //import NotFound from '../components/common/NotFound';
 
 let basename = '/';
+let Router = require('react-router-dom')['BrowserRouter'];
 if (__PROD__) {
   basename = '/douban-movie/'
+  Router = require('react-router-dom')['HashRouter']
 }
 
 const Routes = () => (
